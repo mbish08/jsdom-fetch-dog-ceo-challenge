1afc3f9ababdb1c7e4aceb9d019c9259be59fa07
mbish08 <mdrew8@gmail.com>
2021-03-18 12:00:01 -0700

    commit tree 750b256cae1d9d69c67fe1dcd92875d630ca4d0d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchImages()
+    fetchBreeds()
+})
+
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(result => eachImage(result))
+}
+
+function eachImage(image) {
+    image.message.forEach(image => displayImage(image))
+    // console.log(image)
+}
+
+function displayImage (dogPic) {
+    const container = document.getElementById('dog-image-container')
+    let newDogPic = document.createElement('img')
+    newDogPic.src = dogPic
+    container.appendChild(newDogPic)
+}
+
+function fetchBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(result => eachBreed(result.message))
+    // console.log(result)
+}
+
+function eachBreed (breeds) {
+    // console.log(breeds)
+    const breedId = document.getElementById('dog-breeds')
+    removeChildren(breedId)
+    
+}
+
